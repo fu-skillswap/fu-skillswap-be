c10a5417d259bedcfadf40c7a77f592a3f8f3b41
TamVQ <quangtam2005.lttg@gmail.com>
2026-08-23 14:34:58 +0700

    fix(forum): add missing Index import in ForumPostReaction

--- src/main/java/com/fptu/exe/skillswap/modules/forum/domain/ForumPostReaction.java
+++ src/main/java/com/fptu/exe/skillswap/modules/forum/domain/ForumPostReaction.java
@@ -9,6 +9,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
